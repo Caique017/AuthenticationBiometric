a24c8f595fac14bdd9b6898deef524a5533e682e
Caique017 <caiquen189@gmail.com>
2023-03-21 16:33:43 -0300

    first commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState, useEffect } from 'react';
+import { Text, View, Button, Alert } from 'react-native';
+import * as localAuthentication from 'expo-local-authentication';
+
+import { styles } from './styles';
 
 export default function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  async function verifyAvaiableAuthentication(){
+    const compatible = await localAuthentication.hasHardwareAsync();
+    console.log(compatible);
+
+    const types = await localAuthentication.supportedAuthenticationTypesAsync();
+    console.log(types.map(type => localAuthentication.AuthenticationType[type]));
+  }
+
+  async function handleAuthentication(){
+    const isBiometricEnrolled = await localAuthentication.isEnrolledAsync();
+
+    if(!isBiometricEnrolled){
+      return Alert.alert('Login', 'Nenhuma biometria encontrada. Por favor, cadastre no dispositivo!');
+    }
+
+    const auth = await localAuthentication.authenticateAsync({
+      promptMessage: 'Login com Biometria',
+      fallbackLabel: 'Biometria não reconhecida'
+    });
+
+    setIsAuthenticated(auth.success);
+  }
+
+  useEffect(() => {
+    verifyAvaiableAuthentication();
+  }, [])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>
+        Usuário conectado: { isAuthenticated ? 'Sim' : 'Não'}
+      </Text>
+
+      <Button
+      title="Entrar"
+      onPress={handleAuthentication} 
+      />
+
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ README.md
@@ -0,0 +1,18 @@
+<h1>AuthenticationBiometric</h1>
+
+local biometrics authentication for android
+<hr>
+
+### How to run the project:
+
+**You can clone the project and run it locally following the steps below:**
+
+1. `git clone https://github.com/Caique017/AuthenticationBiometric.git` to clone the project
+2. `npm install` to install project dependencies
+3. Access your command prompt and run `npx expo start` and remembering that you need to be inside the directory you cloned, and on your device you will need to have `expo go`
+4. Once this is done, you will read the qr CODE that will appear on your terminal and run the application
+<hr>
+
+<p>Local biometrics authentication for android made with react native and I used an expo library called LocalAuthentication so that I can do the biometrics validation for our app.</p>
+
+<p> any questions send me a message via linkedin: <a href="https://www.linkedin.com/in/caique-nunes-624720202/" target="_blank">My profile.</a></p>
